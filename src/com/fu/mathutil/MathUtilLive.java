ec993323e36bb8e9b0c036bd9b2a93371fb41987
Thanh2511 <dangquocthanhsaclo@gmail.com>
2021-07-15 19:22:18 +0700

    lan thu 2 lam chuyen ay

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -9,8 +9,10 @@ public class MathUtilLive {
         //expected value: 5! = 120
         //hàm ai đó viết chạy thực tế là mấy - actual, giả sử 120
         //if expected == actual, hàm đó ngon trong tình huống này
-        MathUtil.getFactorial(21);
         System.out.println("Expected: 6! = 720; actual: " + MathUtil.getFactorial(6));
+        //lần đầu sửa code sau lần đầu up code lên server
+        System.out.println("Expected: 3! = 6; actual: " + MathUtil.getFactorial(3));
+        
         System.out.println("Expected: 1! = 1; actual: " + MathUtil.getFactorial(1));
         System.out.println("Expected: 0! = 1; actual: " + MathUtil.getFactorial(0));
         
